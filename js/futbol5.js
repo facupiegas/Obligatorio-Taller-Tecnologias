854ade34a8e35f19cac9f1ab6928da95eb3d88e5
ayurisich <ayurisich@salesforce.com>
2018-02-26 21:58:41 -0300

    ajax para login, signup y lista canchas

--- js/futbol5.js
+++ js/futbol5.js
@@ -1,37 +1,97 @@
-favoritos = [];
+var favoritos = [];
+var idUsuario = -1;
+
 function hacerLogin(){
-    //llamada a la api para verificar que exista el usuario con esa clave
-    //Crear mensaje de error si falla
-    //Si hay exito
-        //1- Guardar nombre usuario: Como evito que se pueda cambiar luego?
-        //2- hacer llamada a traer canchas y partidos, levantar favoritos y cargar paginas necesarias
-        //2- mensaje de bienvenida y mas transiciones
-    //Aca se redirige a la pagina
-    $.mobile.navigate('#listadoCanchas');
+    var usuario = $('#login #usuario').val();
+    var pass = $('#login #clave').val();
+    if(usuario != '' & pass != ''){
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "http://quierojugar.tribus.com.uy/login?user=" + usuario + "&password=" + pass,
+            data:'',            
+            success: function(retorno){
+                idUsuario = retorno.id_usuario;
+                traerCanchas();
+                $.mobile.navigate('#listadoCanchas');
+            },
+            error:function(retorno){
+                $('#login #mensaje').html("<p>"+ retorno.mensaje +"</p>")
+            }
+        })
+    } else {
+        $('#mensaje').html("<p>Ingrese usuario y clave</p>")
+    }
 }
+
 function hacerSignup(){
-    //llamada a la api para verificar que no exista el usuario,
-    //Crear mensaje de error si falla
-    //Si hay exito
-        //1- Guardar nombre usuario: Como evito que se pueda cambiar luego?
-        //2- hacer llamada a traer canchas y partidos, levantar favoritos y cargar paginas necesarias
-        //2- mensaje de bienvenida y mas transiciones
-    //Aca se redirige a la pagina
-    $.mobile.navigate('#listadoCanchas');
+    var usuario = $('#signup #usuario').val();
+    var pass = $('#signup #clave').val();
+    var nombre = $('#signup #nombre').val();
+    if(usuario != '' && pass != '' && nombre != ''){
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "http://quierojugar.tribus.com.uy/registrar?user=" + usuario + "&password=" + pass + '&nombre='+ nombre,
+            data:'',            
+            success: function(retorno){
+                idUsuario = retorno.id_usuario;
+                traerCanchas();
+                $.mobile.navigate('#listadoCanchas');
+            },
+            error:function(retorno){
+                $('#login #mensaje').html("<p>"+ retorno.mensaje +"</p>")
+            }
+        })
+    } else {
+        $('#mensaje').html("<p>Ingrese usuario, clave y nombre de usuario</p>")
+    }
 }
+
 function hacerLogout(){
     //Borrar sesion del usuario
-    //Transiciones
+    idUsuario = -1;
     //redirigir a login
     $.mobile.navigate('#login');
 }
-//Al cargar las paginas siempre debo checkear que estoy logeado y sino redirigirme al login
-    //evento beforepagechange puede checkear si estoy logeado.
-function traerCanchasyPartidos(){
 
+function traerCanchas(){
+    if(idUsuario != -1){
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "http://quierojugar.tribus.com.uy/getCanchas",
+            data:'',            
+            success: function(retorno){
+                var lista = $("#listadoCanchas #canchas");
+                lista.empty();
+                var largo = retorno.canchas.length;
+                var i;
+                for(i=0; i<largo; i++){
+                    lista.append(
+                        "<li><a href='#detalleCancha' id='" + retorno.canchas[i].nombre + "'>" + retorno.canchas[i].nombre + "</a>"
+                        + "<a data-cancha='"+retorno.canchas[i].nombre+"' href='#' onclick='guardarCanchaFavorita($(this))'></a></li>"
+                    );
+                }
+                lista.listview('refresh');
+                marcarFavoritos();
+                $.mobile.navigate('#listadoCanchas');
+            },
+            error:function(retorno){
+                $('#login #mensaje').html("<p>"+ retorno.mensaje +"</p>")
+            }
+        })
+    } else {
+        $.mobile.navigate('#login');
+    }
 }
+
+function marcarFavoritos(){
+    favoritos = 
+}
+
 function guardarCanchaFavorita(esto){
-    var id = esto.prev().attr('id');
+    var id = esto.data('cancha');
     if(favoritos.indexOf(id) == -1){
         favoritos.push(id);
         esto.removeClass('ui-icon-star');
