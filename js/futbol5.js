d0c5129cf06f0808528943f5c12e929db315f8bd
Facundo Piegas <facundo.piegas@gmail.com>
2018-02-28 18:56:09 -0300

    Login y registo
    Login y registro de nuevo usuario funcionando

--- js/futbol5.js
+++ js/futbol5.js
@@ -1,4 +1,21 @@
+favoritos = [];
 function hacerLogin(){
+  var usuarioLogin = $("#usuarioLogin").val();
+  var claveLogin = $("#claveLogin").val()
+  if(usuarioLogin != "" && claveLogin !=""){
+    $.ajax({
+      url:'http://quierojugar.tribus.com.uy/login',
+      type:'GET',
+      dataType:'json',
+      data: {user:usuarioLogin, password:claveLogin},
+      success: function(response){
+          $.mobile.navigate('#listadoCanchas');
+      },
+      error: function(err)
+      {
+        console.log("error! -> " + err);
+      }
+    });
     //llamada a la api para verificar que exista el usuario con esa clave
     //Crear mensaje de error si falla
     //Si hay exito
@@ -6,9 +23,34 @@ function hacerLogin(){
         //2- hacer llamada a traer canchas y partidos, levantar favoritos y cargar paginas necesarias
         //2- mensaje de bienvenida y mas transiciones
     //Aca se redirige a la pagina
-    $.mobile.navigate('#listadoCanchas');
+    }
+    $("#messageLogin").text("(*) Por favor complete todos los campos requeridos");
 }
-function hacerSignup(){
+function irARegistro(){
+    $.mobile.navigate('#registro');
+}
+function hacerRegistro(){
+  var nombre = $("#nombreNew").val();
+  var username = $("#usuarioNew").val();
+  var password = $("#claveNew").val();
+  if(nombre != "" && username !="" && password != ""){
+    $.ajax({
+      url:'http://quierojugar.tribus.com.uy/registrar ',
+      type:'POST',
+      dataType:'json',
+      data: {user:username, password:password, nombre:nombre},
+      success: function(response){
+          //vacio variables y redirecciono a login
+          $.mobile.navigate('#login');
+          //doy aviso de registo existoso
+        //$("#mensaje").text(response);
+      },
+      error: function(err)
+      {
+        console.log("error! -> " + err);
+      }
+    });
+  }
     //llamada a la api para verificar que no exista el usuario,
     //Crear mensaje de error si falla
     //Si hay exito
@@ -16,7 +58,7 @@ function hacerSignup(){
         //2- hacer llamada a traer canchas y partidos, levantar favoritos y cargar paginas necesarias
         //2- mensaje de bienvenida y mas transiciones
     //Aca se redirige a la pagina
-    $.mobile.navigate('#listadoCanchas');
+    $("#messageRegister").text("(*) Por favor complete todos los campos requeridos");
 }
 function hacerLogout(){
     //Borrar sesion del usuario
@@ -29,9 +71,33 @@ function hacerLogout(){
 function traerCanchasyPartidos(){
 
 }
-function guardarCanchaFavorita(){
-
+function guardarCanchaFavorita(esto){
+    var id = esto.prev().attr('id');
+    if(favoritos.indexOf(id) == -1){
+        favoritos.push(id);
+        esto.removeClass('ui-icon-star');
+        esto.addClass('ui-icon-staryellow');
+        esto.attr('onclick', 'borrarCanchaFavorita($(this))')
+    }
+    guardarFavoritos(favoritos);
+}
+function borrarCanchaFavorita(esto){
+    var id = esto.prev().attr('id');
+    var index = favoritos.indexOf(id);
+    if(index > -1){
+        favoritos.splice(index, 1);
+        esto.removeClass('ui-icon-staryellow');
+        esto.addClass('ui-icon-star');
+        esto.attr('onclick', 'guardarCanchaFavorita($(this))')
+    }
+    guardarFavoritos(favoritos);
+}
+function borrarDeFavoritos(esto){
+    esto.closest('li').remove();
+}
+function guardarFavoritos(array){
+    //Funcion que guarda con SQL los favoritos en la base de datos local
 }
 function traerCanchasFavoritas(){
-
+    //Funcion que trae los favoritos de la base de datos local
 }
